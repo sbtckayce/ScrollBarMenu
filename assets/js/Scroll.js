24a9c7fff400d6b34ee4352395bd06c7ae99401e
lab6TranTrungKien <19130110@st.hcmuaf.edu.vn>
2022-03-23 11:18:50 +0700

    first commit

---
+++ assets/js/Scroll.js
@@ -0,0 +1,8 @@
+let progress = document.querySelector('#progressbar');
+
+let totalHeight = document.body.scrollHeight-window.innerHeight;
+
+window.onscroll=function(){
+    let progressHeight =(window.pageYOffset/totalHeight)*100;
+    progress.style.height=progressHeight+"%";
+}
